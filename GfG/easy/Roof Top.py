782e92cb2812dddd97eb08f7c4491ec220b29fab
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2024-09-26 00:16:53 +0530

    Create Roof Top.py

---
+++ GfG/easy/Roof Top.py
@@ -0,0 +1,17 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find maximum number of consecutive steps 
+    #to gain an increase in altitude with each step.
+    def maxStep(self, arr):
+        #Your code here
+        jump=0
+        temp=0
+        for i in range(1,len(arr)):
+            if arr[i]>arr[i-1]:
+                temp+=1
+            else:
+                jump=max(jump,temp)
+                temp=0
+        return max(jump,temp)
