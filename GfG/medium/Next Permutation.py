0c839c32146fcccaaab0d45d3a4e201333857805
JayKumarLal <143897429+AxelBlazerGit@users.noreply.github.com>
2024-11-19 00:59:54 +0530

    Create Next Permutation.py

---
+++ GfG/medium/Next Permutation.py
@@ -0,0 +1,32 @@
+#User function Template for python3
+
+class Solution:
+    
+    def nextPermutation(self, arr):
+        # code here
+        # find first decreasing element from the right [i]
+        # find smallest element larger than arr[i] to swap
+        # reverse after swapped index [i+1] to [end]
+        def rev(s,e):
+            while s<e:
+                arr[s],arr[e]=arr[e],arr[s]
+                s+=1
+                e-=1
+                
+        n=len(arr)
+        i=n-2
+        
+        while i>=0 and arr[i]>=arr[i+1]:
+            i-=1
+            
+        if i>=0:
+            j=n-1
+            
+            while arr[j]<=arr[i]:
+                j-=1
+                
+            arr[i],arr[j]=arr[j],arr[i]
+            
+        rev(i+1,n-1)
+        
+        return arr
